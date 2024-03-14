aa2082817b084ce15d4c7897dbb009115785092e
TUoS Student <student@TEN00BE4391A1AA.shefuniad.shef.ac.uk>
2024-03-14 12:59:44 +0000

    updated explore.py

--- src/explore.py
+++ src/explore.py
@@ -5,14 +5,20 @@ import rospy
 from std_msgs.msg import String 
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Odometry
 
-class Publisher(): 
+class Explorer(): 
+
+    def callback(self, topic_data: Odometry):
+        pose = topic_data.pose.pose
+        return pose.position
+
 
     def __init__(self): 
         self.node_name = "explore" # name of initialised node
         topic_name = "/move_base_simple/goal" # the message to publish
         self.pub = rospy.Publisher(topic_name, PoseStamped, queue_size=10) 
-        
+        self.sub = rospy.Subscriber("odom", Odometry, self.callback)       
         self.pub2 = rospy.Publisher("cmd_vel", Twist, queue_size=10) 
         rospy.init_node(self.node_name, anonymous=True) 
         self.rate = rospy.Rate(10) 
@@ -33,25 +39,35 @@ class Publisher():
 
     def main_loop(self):
 
-        
-
-        set_position = PoseStamped()
-
-        set_position.header.frame_id = "map"
-
-        set_position.pose.position.x = 0
-        set_position.pose.position.y = 1
-        set_position.pose.orientation.w = 1
+        def set_position(message,x,y):
+            message.header.frame_id = "map"
+            message.pose.position.x = x
+            message.pose.position.y = y
+            message.pose.orientation.w = 1
+            return message
 
 
+        coordinate = PoseStamped()
+
+        currentPosition = Odometry().pose.pose.position
 
         while not self.ctrl_c: 
-            self.pub.publish(set_position)
-            self.rate.sleep()
+            self.pub.publish(set_position(coordinate,1.25,1.25))
+            if (currentPosition.x == 1.25) and (currentPosition.y == 1.25):
+                self.pub.publish(set_position(coordinate,-1.25,1.25))
+                if (currentPosition.x == -1.25) and (currentPosition.y == 1.25):
+                    self.pub.publish(set_position(coordinate,-1.25,-1.25))
+                    if (currentPosition.x == -1.25) and (currentPosition.y == -1.25):
+                        self.pub.publish(set_position(coordinate,1.25,-1.25))
 
 if __name__ == '__main__': 
-    publisher_instance = Publisher() 
+    publisher_instance = Explorer() 
     try:
         publisher_instance.main_loop() 
     except rospy.ROSInterruptException:
         pass
+
+
+
+
+
