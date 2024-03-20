2d6ff6e2c79595e2ff4f2bf40c1aaa216a7fa979
TUoS Student <student@TEN54BF6461A7A0.shefuniad.shef.ac.uk>
2024-03-20 13:34:01 +0000

    functioning explore.py script for moving to a given list of points

--- src/explore.py
+++ src/explore.py
@@ -5,15 +5,22 @@ import rospy
 from std_msgs.msg import String 
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Odometry
 
-class Publisher(): 
+class Explorer(): 
+
+    def callback(self, data: Odometry):
+        self.pose = data.pose.pose
+        return self.pose.position
+
 
     def __init__(self): 
         self.node_name = "explore" # name of initialised node
         topic_name = "/move_base_simple/goal" # the message to publish
         self.pub = rospy.Publisher(topic_name, PoseStamped, queue_size=10) 
-        
-        self.pub2 = rospy.Publisher("cmd_vel", Twist, queue_size=10) 
+        self.sub = rospy.Subscriber("odom", Odometry, self.callback)       
+        self.pub2 = rospy.Publisher("cmd_vel", Twist, queue_size=10)
+        self.pub3 = rospy.Publisher("explore_debug", String, queue_size=10)
         rospy.init_node(self.node_name, anonymous=True) 
         self.rate = rospy.Rate(10) 
 
@@ -33,25 +40,59 @@ class Publisher():
 
     def main_loop(self):
 
-        
+        t = 0.1 # coordinate tolerance
 
-        set_position = PoseStamped()
+        def set_position(message,x,y):
+            message.header.frame_id = "map"
+            message.pose.position.x = x
+            message.pose.position.y = y
+            message.pose.orientation.w = 1
+            return message
 
-        set_position.header.frame_id = "map"
+        # def convert2ASCII(data_string):
+        #     ascii_text = ''.join(chr(int(data, 2)) for data in str(data_string).split())
+        #     return ascii_text
 
-        set_position.pose.position.x = 0
-        set_position.pose.position.y = 1
-        set_position.pose.orientation.w = 1
+        def check_position(position,x,y):
+            return (x - t <= position.x <= x + t) and (y - t <= position.y <= y + t)
 
+        locations = [[1.25,1.25],[-1.25,1.25],[-1.25,-1.25],[1.25,-1.25]]
 
+        visit_no = 0
+
+        coordinate = PoseStamped()
+
+        currentPosition = Odometry().pose.pose.position
 
         while not self.ctrl_c: 
-            self.pub.publish(set_position)
-            self.rate.sleep()
+
+            if visit_no == 4:
+                visit_no = 0
+
+            self.pub.publish(set_position(coordinate,locations[visit_no][0],locations[visit_no][1]))
+
+            if check_position(self.pose.position,locations[visit_no][0],locations[visit_no][1]):
+                visit_no += 1
+
+            # if location == 0 and check_position(self.pose.position,0,0):
+            #     self.pub.publish(set_position(coordinate,1.25,1.25))
+            #     # self.pub3.publish(str(check_position(self.pose.position,1.25,1.25)))
+            # elif check_position(self.pose.position,1.25,1.25):
+            #     location += 1
+            #     self.pub.publish(set_position(coordinate,-1.25,1.25))
+            #     if check_position(self.pose.position,-1.25,1.25):
+            #         self.pub.publish(set_position(coordinate,-1.25,-1.25))
+            #         if check_position(self.pose.position,-1.25,-1.25):
+            #             self.pub.publish(set_position(coordinate,1.25,-1.25))
 
 if __name__ == '__main__': 
-    publisher_instance = Publisher() 
+    publisher_instance = Explorer() 
     try:
         publisher_instance.main_loop() 
     except rospy.ROSInterruptException:
         pass
+
+
+
+
+
